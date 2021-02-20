875e6a66ea63acab8332a957dc9fef0c1d360ef6
Mark Dragnev <m.n.dragnev@student.tudelft.nl>
2021-02-21 01:43:36 +0200

    fixed

--- app/12d/main/main.py
+++ app/12d/main/main.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from scraper import scaperFunction
 
 url = "https://raw.githubusercontent.com/nmapx/revolut-stocks-list/master/list.csv"
 df_stocks = pd.read_csv(url)
+
+data = scaperFunction(5)
+print(data)
