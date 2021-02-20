c1b48666c8b739d20d41e1066df07064dd225bae
ynngdd <yaskata123@gmail.com>
2021-02-21 00:39:44 +0100

    added scraping function

--- app/12d/main/main.py
+++ app/12d/main/main.py
@@ -2,5 +2,3 @@ import pandas as pd
 
 url = "https://raw.githubusercontent.com/nmapx/revolut-stocks-list/master/list.csv"
 df_stocks = pd.read_csv(url)
-
-
--- app/12d/scraper/scraper.py
+++ app/12d/scraper/scraper.py
@@ -1,12 +1,30 @@
 import praw
 import pandas as pd
 import datetime as dt
-reddit = praw.Reddit(
-    client_id="qMLQNap3Yn5knA",
-    client_secret="tNGtBkbwD4wOzaWszjRvDwM5_ReH6g",
-    user_agent="StockScraper"
-)
-WSB = reddit.subreddit('wallstreetbets')
 
-for submission in WSB.hot(limit=10):
-    print(submission.title)
+
+def scaperFunction(numberOfPosts):
+    reddit = praw.Reddit(
+        client_id="qMLQNap3Yn5knA",
+        client_secret="tNGtBkbwD4wOzaWszjRvDwM5_ReH6g",
+        user_agent="StockScraper"
+    )
+    WSB = reddit.subreddit('wallstreetbets')
+
+    topics_dict = {"title": [], \
+                   "score": [], \
+                   "id": [], "url": [], \
+                   "comms_num": [], \
+                   "created": [], \
+                   "body": []}
+
+    for submission in WSB.hot(limit=numberOfPosts):
+        topics_dict["title"].append(submission.title)
+        topics_dict["score"].append(submission.score)
+        topics_dict["id"].append(submission.id)
+        topics_dict["url"].append(submission.url)
+        topics_dict["comms_num"].append(submission.num_comments)
+        topics_dict["created"].append(submission.created)
+        topics_dict["body"].append(submission.selftext)
+
+    return topics_dict
