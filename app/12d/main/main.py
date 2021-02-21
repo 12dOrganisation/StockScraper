db0dc79b7b34ad4340bb4f41166280a17b75e7cf
ynngdd <yaskata123@gmail.com>
2021-02-21 01:05:06 +0100

    formatted csv stock table

--- app/12d/main/main.py
+++ app/12d/main/main.py
@@ -3,6 +3,8 @@ from scraper import scaperFunction
 
 url = "https://raw.githubusercontent.com/nmapx/revolut-stocks-list/master/list.csv"
 df_stocks = pd.read_csv(url)
-
-data = scaperFunction(5)
-print(data)
+df_stocks.loc[-1] = [df_stocks.columns[0], df_stocks.columns[1]]  # adding a row
+df_stocks.index = df_stocks.index + 1  # shifting index
+df_stocks.sort_index(inplace=True)
+df_stocks = df_stocks.rename(columns={'A':'stock_index', 'Agilent Technologies': 'stock_name'})
+print(df_stocks)
