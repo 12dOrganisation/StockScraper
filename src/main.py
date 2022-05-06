c6d68633b9e52b8924dd1098d76aac111bcd7120
Alexander Stoilov <a.a.stoilov@gmail.com>
2022-05-06 14:39:48 +0300

    ‎

---
+++ src/main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from scraper import scraperFunction
+
+url = "https://raw.githubusercontent.com/nmapx/revolut-stocks-list/master/list.csv"
+df_stocks = pd.read_csv(url)
+df_stocks.loc[-1] = [df_stocks.columns[0], df_stocks.columns[1]]  # adding a row
+df_stocks.index = df_stocks.index + 1  # shifting index
+df_stocks.sort_index(inplace=True)
+df_stocks = df_stocks.rename(columns={'A': 'stock_index', 'Agilent Technologies': 'stock_name'})
+
+df_scraped = pd.DataFrame(scraperFunction(1000))
+
+ser = pd.Series(data=0, index=df_stocks.index)
+
+for index, row in df_stocks.iterrows():
+    for index1, row1 in df_scraped.iterrows():
+        temp = row1['title'].split(" ")
+        temp1 = row1['body'].split(" ")
+        dollar_sign = "$"
+        dollar_sign += row['stock_index']
+        if (row['stock_index'] in (*temp, *temp1)) \
+                or (row['stock_name'] in (*row1['title'], *row1['body'])) \
+                or (dollar_sign in (*temp, *temp1)):
+            # print(row['stock_index'])
+            # print(row1['title'])
+
+            ser[index] = ser[index] + row1["comms_num"]
+            # print(ser[index], index)
+
+df_stocks["nr_comments"] = ser
+
+to_export = df_stocks[df_stocks["nr_comments"] > 0].sort_values("nr_comments", ascending=False)
+exported = to_export.to_csv('hot_stocks.csv')
---
+++ src/scraper_function.py
@@ -0,0 +1,33 @@
+import praw
+import pandas as pd
+import datetime as dt
+
+
+def scraperFunction(numberOfPosts):
+    reddit = praw.Reddit(
+        client_id="qMLQNap3Yn5knA",
+        client_secret="tNGtBkbwD4wOzaWszjRvDwM5_ReH6g",
+        user_agent="StockScraper"
+    )
+    WSB = reddit.subreddit('wallstreetbets')
+
+    topics_dict = {
+        "title": [],
+        "score": [],
+        "id": [],
+        "url": [],
+        "comms_num": [],
+        "created": [],
+        "body": []
+    }
+
+    for submission in WSB.new(limit=numberOfPosts):
+        topics_dict["title"].append(submission.title)
+        topics_dict["score"].append(submission.score)
+        topics_dict["id"].append(submission.id)
+        topics_dict["url"].append(submission.url)
+        topics_dict["comms_num"].append(submission.num_comments)
+        topics_dict["created"].append(submission.created)
+        topics_dict["body"].append(submission.selftext)
+
+    return topics_dict
